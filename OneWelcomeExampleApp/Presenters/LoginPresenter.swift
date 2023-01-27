3299f2d8858524fa26fecc103d1e248374630d76
Konrad Falkowski <konrad.falkowski@onewelcome.com>
2023-01-27 15:15:33 +0100

    OAD-287 app closes

--- OneWelcomeExampleApp/Presenters/LoginPresenter.swift
+++ OneWelcomeExampleApp/Presenters/LoginPresenter.swift
@@ -14,6 +14,7 @@
 // limitations under the License.
 
 import UIKit
+import OneginiSDKiOS
 
 typealias LoginPresenterProtocols = LoginPresenterProtocol & LoginViewDelegate & LoginInteractorDelegate
 
